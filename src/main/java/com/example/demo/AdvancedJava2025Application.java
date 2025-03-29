e49b32c288f878b6d42358eb25d62f90bea59044
egorycheva28 <egor.masha28@list.ru>
2025-03-29 10:11:12 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/AdvancedJava2025Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJava2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJava2025Application.class, args);
+	}
+
+}
