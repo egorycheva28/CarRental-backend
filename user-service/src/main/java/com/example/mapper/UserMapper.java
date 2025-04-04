1664a1ddd2cd5961b2e69e93df61a84a12fe0f4d
egorycheva28 <egor.masha28@list.ru>
2025-04-04 12:01:14 +0700

    преобразование данных для регистрации пользователей

---
+++ user-service/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.example.mapper;
+
+import com.example.dto.requests.RegisterUser;
+import com.example.model.User;
+
+public class UserMapper {
+
+    public static User registerUser(RegisterUser registerUser) {
+        User user = new User();
+        user.setLastName(registerUser.lastName());
+        user.setFirstName(registerUser.firstName());
+        user.setMiddleName(registerUser.middleName());
+        user.setAge(registerUser.age());
+        user.setPhone(registerUser.phone());
+        user.setEmail(registerUser.email());
+        user.setPassword(registerUser.password());
+
+        return user;
+    }
+
+    //public static GetUser getUser(User user){}
+}
