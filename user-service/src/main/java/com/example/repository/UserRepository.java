45cd50d84e4ac5ed19bf08f727f5b780a28e7df2
egorycheva28 <egor.masha28@list.ru>
2025-04-04 12:09:26 +0700

    добавлены поиск по email и номеру телефона, проверка на существование email и номера телефона

--- user-service/src/main/java/com/example/repository/UserRepository.java
+++ user-service/src/main/java/com/example/repository/UserRepository.java
@@ -4,9 +4,16 @@ import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 
+    Optional<User> findByPhone(String phone);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
 }
