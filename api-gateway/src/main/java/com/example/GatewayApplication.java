a735d85a77d5ac7e56bdb046ca088901645fbff5
egorycheva28 <egor.masha28@list.ru>
2025-06-11 15:04:35 +0700

    модуль для gateway

---
+++ api-gateway/src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/*public class GatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
